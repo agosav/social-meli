3c8214b1b169fe33aab70bcc671dbbbc4fb9522a
Carolina Comba <carolinacomba42@gmail.com>
2025-02-05 22:10:24 -0300

    Update UserControllerTest.java

--- src/test/java/com/socialmeli/socialmeli/controllers/UserControllerTest.java
+++ src/test/java/com/socialmeli/socialmeli/controllers/UserControllerTest.java
@@ -1,15 +1,24 @@
 package com.socialmeli.socialmeli.controllers;
 
+import com.socialmeli.socialmeli.dto.response.MessageDto;
+import com.socialmeli.socialmeli.enums.Message;
+import com.socialmeli.socialmeli.models.Follow;
 import com.socialmeli.socialmeli.models.User;
+import com.socialmeli.socialmeli.services.IUserService;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +29,9 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Mock
+    private IUserService userService;
 
     @Test
     @DisplayName("followToUserTest")
@@ -39,5 +51,24 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.followed").isArray())
                 .andExpect(jsonPath("$.followed[0].user_id").value(user2.getId()))
                 .andExpect(jsonPath("$.followed[0].user_name").value(user2.getName()));
+    }
+
+    @Test
+    @DisplayName("unfollowToUserTest")
+    public void unfollowUserTest() throws Exception {
+        // Arrange
+        User user1 = new User(1, "Agostina Avalle", false);
+        User user2 = new User(2, "Carolina Comba", true);
+        Integer userId = 1;
+        Integer userIdToUnfollow = 2;
+        String expectedMessage = Message.USER_UNFOLLOWED.format(user2.getName());
+
+        when(userService.unfollow(userId, userIdToUnfollow)).thenReturn(new MessageDto(expectedMessage));
+
+        // Act & Assert
+        mockMvc.perform(post("/" + userId + "/unfollow/" + userIdToUnfollow)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(expectedMessage));
     }
 }
